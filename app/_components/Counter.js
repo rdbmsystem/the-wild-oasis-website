547309d889e80cd86fa49cd209ead23c5ac11d1d
rdbmsystem <rdbmsystem@gmail.com>
2024-10-06 11:33:28 +0800

    Initial commit

---
+++ app/_components/Counter.js
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter({ user }) {
+  const [count, setCount] = useState(0);
+
+  console.log(user);
+
+  return (
+    <div>
+      <p>There are {user.length} users</p>
+      <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+    </div>
+  );
+}
